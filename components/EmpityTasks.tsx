53a6bf95fe499dffd4438c6f7fa7da9efd66f088
EvanioTech <evanioshark@gmail.com>
2025-11-21 21:30:20 -0300

    init component empityTasks

---
+++ components/EmpityTasks.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EmpityTasks = () => {
+  return (
+    <View>
+      <Text>EmpityTasks</Text>
+    </View>
+  )
+}
+
+export default EmpityTasks
