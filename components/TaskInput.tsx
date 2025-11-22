1508fa5d70b6ee2ecfbb78f3bd3832398406a51c
Evanio Tech <evanioshark@gmail.com>
2025-11-22 07:54:53 -0300

    create component taskInput

---
+++ components/TaskInput.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Modal } from 'react-native'
+import React from 'react'
+
+const TaskInput = () => {
+  return (
+    <Modal>
+      <View>
+        <Text>TaskInput</Text>
+      </View>
+    </Modal>
+  )
+}
+
+export default TaskInput
